316a7d8847cebd155f14fad0f090ec8e2ee34723
albf <albf.unicamp@gmail.com>
2015-06-06 06:12:06 -0300

    refactor

--- lab3/dce-pass-p3/p3/Liveness.cpp
+++ lab3/dce-pass-p3/p3/Liveness.cpp
@@ -1,13 +1,10 @@
 #include <algorithm>
-#include <fstream>
-#include <sstream>
 #include <vector>
 
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instruction.h"
-#include "llvm/IR/Instructions.h"
 #include "llvm/IR/IntrinsicInst.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/CFG.h"
@@ -20,6 +17,54 @@ using namespace std;
 #define debug true
 
 namespace {
+    // Makes Union of vectors.
+    vector<Instruction *> Difference(vector<Instruction *>& vec_1, vector<Instruction *>& vec_2) {
+        vector<Instruction *> u;
+
+        for(vector<Instruction*>::iterator elem = vec_1.begin(); elem != vec_1.end(); elem++) {
+            if(find(vec_2.begin(), vec_2.end(), *elem) == vec_2.end()) {
+                u.push_back(*elem);
+            }
+        }
+       //errs() << "u_size : " << u.size() << "\n";
+       return u; 
+    }
+
+    // Makes difference of vectors.
+    vector<Instruction *> Union(vector<Instruction *>& vec_1, vector<Instruction *>& vec_2) {
+        vector<Instruction *> u;
+
+        for(vector<Instruction*>::iterator elem = vec_1.begin(); elem != vec_1.end(); elem++) {
+            u.push_back(*elem);
+        }
+        for(vector<Instruction*>::iterator elem = vec_2.begin(); elem != vec_2.end(); elem++) {
+            if(find(vec_1.begin(), vec_1.end(), *elem) == vec_1.end()) {
+                u.push_back(*elem);
+            }
+        }
+       return u; 
+    }
+
+    // Makes union of difference of 3 vectors. Returns = vec_0 U (vec_1 - vec_2)
+    vector<Instruction *> UnionOfDifference(vector<Instruction *>& vec_0, vector<Instruction *>& vec_1, vector<Instruction *>& vec_2) {
+        vector<Instruction *> u;
+
+        for(vector<Instruction*>::iterator elem = vec_1.begin(); elem != vec_1.end(); elem++) {
+            u.push_back(*elem);
+        }
+        for(vector<Instruction*>::iterator elem = vec_2.begin(); elem != vec_2.end(); elem++) {
+            if(find(vec_1.begin(), vec_1.end(), *elem) == vec_1.end()) {
+                u.push_back(*elem);
+            }
+        }
+        for(vector<Instruction*>::iterator elem = vec_0.begin(); elem != vec_0.end(); elem++) {
+            if(find(u.begin(), u.end(), *elem) == u.end()) {
+                u.push_back(*elem);
+            }
+        }
+       return u; 
+    }
+
     class VecI {                        // Instruction vectors.
         public:
             vector<Instruction*> use; 
@@ -35,127 +80,87 @@ namespace {
 
     class VecL {                        // Live vectors.    
         public:
-            DenseMap<BasicBlock*, VecB*> b_vecs;
             DenseMap<Instruction*, VecI*> i_vecs;
+            DenseMap<BasicBlock*, VecB*> b_vecs;
 
             // Destructor
             ~VecL() {
-                for(DenseMap< BasicBlock*, VecB* >::iterator i = b_vecs.begin();  i != b_vecs.end(); i++) { 
+                // New/Alloc elements in second position of DenseMap.
+                for(DenseMap<BasicBlock*, VecB*>::iterator i = b_vecs.begin();  i != b_vecs.end(); i++) { 
                     delete i->second;
                 }
 
                 b_vecs.clear();
 
-                for(DenseMap< Instruction*, VecI* >::iterator i = i_vecs.begin();  i != i_vecs.end(); i++) {
+                for(DenseMap<Instruction*, VecI*>::iterator i = i_vecs.begin();  i != i_vecs.end(); i++) {
                     delete i->second;
                 }
 
                 i_vecs.clear();
             }
 
-            void bAdd(BasicBlock* block) {
-                b_vecs[block] = new VecB();
-
-                for(succ_iterator succesor = succ_begin(block); succesor != succ_end(block); succesor++) {
-                    BasicBlock* Succ = *succesor;
-                    b_vecs[block]->suc.push_back(Succ);
-                }
+            void iAdd(Instruction* new_i) {
+                i_vecs[new_i] = new VecI();
             }
 
-            void iAdd(Instruction* inst) {
-                i_vecs[inst] = new VecI();
+            void bAdd(BasicBlock* new_b) {
+                b_vecs[new_b] = new VecB();
+
+                for(succ_iterator succ = succ_begin(new_b); succ != succ_end(new_b); succ++) {
+                    b_vecs[new_b]->suc.push_back((BasicBlock *) *succ);
+                }
             }
     };
 
-    struct dcep3 : public FunctionPass
+    struct livinessP3 : public FunctionPass
     {
         static char ID;
         int removalCount;
 
-        dcep3() : FunctionPass(ID) {
+        livinessP3() : FunctionPass(ID) {
             removalCount=0;
         }
 
-        ~dcep3() {
+        ~livinessP3() {
+            errs() << "Total Instructions Removed: " << removalCount << "\n";
         }
 
-        // Makes Union of vectors.
-        vector<Instruction *> Difference(vector<Instruction *>& vec_1, vector<Instruction *>& vec_2) {
-            vector<Instruction *> u;
-
-            for(vector<Instruction*>::iterator elem = vec_1.begin(); elem != vec_1.end(); elem++) {
-                if(find(vec_2.begin(), vec_2.end(), *elem) == vec_2.end()) {
-                    u.push_back(*elem);
-                }
-            }
-           //errs() << "u_size : " << u.size() << "\n";
-           return u; 
-        }
-
-        // Makes difference of vectors.
-        vector<Instruction *> Union(vector<Instruction *>& vec_1, vector<Instruction *>& vec_2) {
-            vector<Instruction *> u;
-
-            for(vector<Instruction*>::iterator elem = vec_1.begin(); elem != vec_1.end(); elem++) {
-                u.push_back(*elem);
-            }
-            for(vector<Instruction*>::iterator elem = vec_2.begin(); elem != vec_2.end(); elem++) {
-                if(find(vec_1.begin(), vec_1.end(), *elem) == vec_1.end()) {
-                    u.push_back(*elem);
-                }
-            }
-           return u; 
-        }
-
-
-        vector<Instruction *> UnionOfDifference(vector<Instruction *>& vec_0, vector<Instruction *>& vec_1, vector<Instruction *>& vec_2) {
-            vector<Instruction *> u;
-
-            for(vector<Instruction*>::iterator elem = vec_1.begin(); elem != vec_1.end(); elem++) {
-                u.push_back(*elem);
-            }
-            for(vector<Instruction*>::iterator elem = vec_2.begin(); elem != vec_2.end(); elem++) {
-                if(find(vec_1.begin(), vec_1.end(), *elem) == vec_1.end()) {
-                    u.push_back(*elem);
-                }
-            }
-            for(vector<Instruction*>::iterator elem = vec_0.begin(); elem != vec_0.end(); elem++) {
-                if(find(u.begin(), u.end(), *elem) == u.end()) {
-                    u.push_back(*elem);
-                }
-            }
-           return u; 
-        }
-
-        // Perform liveness analisys and save results in allVecs.
-        void livenessAnalysis(Function* func, VecL& allVecs) {
-            unsigned operands, opr, last_size;
+        // Perform liveness analisys and remove Dead Instructions.
+        virtual bool runOnFunction(Function &F) {
+            bool changed = false;
             bool redoIn = true;
+            unsigned operands, opIt, last_size;
             Value * aValue;
             Instruction * aInstruction; 
             VecB * b, * succ;
+            VecL allVecs;
+            vector<Instruction*> removal;
+
+            if(debug) {
+                errs() << "Function: " << F.getName().str() << "\n";
+            }
 
             // Part 1: Find .use and .def vectors for Blocks.
            
-            for(Function::iterator it = func->begin(); it != func->end(); ++it) {           // Iterate in b_vecs
+            for(Function::iterator it = F.begin(); it != F.end(); ++it) {           // Iterate in b_vecs
                 allVecs.bAdd(&*it);
 
-                for (BasicBlock::iterator jt = it->begin(); jt != it->end(); ++jt) {        // Iterate in i_vecs
-                    if (isa < Instruction >(*jt)) {
+                for(BasicBlock::iterator jt = it->begin(); jt != it->end(); ++jt) {        // Iterate in i_vecs
+                    if (isa <Instruction>(*jt)) {
                         allVecs.iAdd(&*jt);
                     }
                 }
             }  
 
-            for (Function::iterator i = func->begin(), e = func->end(); i != e; ++i) {
+            for(Function::iterator i = F.begin(), e = F.end(); i != e; ++i) {
                 b = allVecs.b_vecs[&*i];
-                for (BasicBlock::iterator j = i->begin(), e = i->end(); j != e; ++j) {
+                for(BasicBlock::iterator j = i->begin(), e = i->end(); j != e; ++j) {
                     operands = j->getNumOperands();
 
-                    for (opr = 0; opr < operands; opr++) {
-                        aValue = j->getOperand (opr);
+                    for(opIt = 0; opIt < operands; opIt++) {
+                        aValue = j->getOperand (opIt);
                         if (isa <Instruction> (*aValue)) {
-                            aInstruction = cast < Instruction > (&*aValue);
+                            aInstruction = cast <Instruction> (&*aValue);
                             
                             if((find(b->def.begin(), b->def.end(), (&*aInstruction)) == b->def.end())
                                 && (find(b->use.begin(), b->use.end(), (&*aInstruction)) == b->use.end())) {
@@ -179,13 +184,13 @@ namespace {
 
             while (redoIn == true) {                 // Loop until IN remain unchanged.
                 redoIn = false;
-                Function::iterator fe = func->end();    // Start at the end.
+                Function::iterator fe = F.end();    // Start at the end.
 
-                while (fe != func->begin()) {
+                while (fe != F.begin()) {
                     fe--;
                     b = allVecs.b_vecs[&*fe];
 
-                    for (unsigned int s = 0; s < b->suc.size(); s++) {    // get successors.
+                    for(unsigned int s = 0; s < b->suc.size(); s++) {    // get successors.
                         succ = allVecs.b_vecs[b->suc[s]];
 
                         for(vector<Instruction*>::iterator elem = succ->in.begin(); elem != succ->in.end(); elem++) {   // Join .in vectors.
@@ -213,9 +218,8 @@ namespace {
                 }
             }
 
-
-            for(Function::iterator i = func->begin(); i != func->end(); i++) {
-                for (BasicBlock::iterator j = i->begin(); j != i->end(); j++) {
+            for(Function::iterator i = F.begin(); i != F.end(); i++) {
+                for(BasicBlock::iterator j = i->begin(); j != i->end(); j++) {
                     if(isa<Instruction>(*j)) {
                         operands = j->getNumOperands();
 
@@ -239,7 +243,7 @@ namespace {
 
             }
 
-            for(Function::iterator i = func->begin(); i != func->end(); i++) {
+            for(Function::iterator i = F.begin(); i != F.end(); i++) {
                 BasicBlock::iterator j = i->end();
                 j--;
                 allVecs.i_vecs[&*j]->out = allVecs.b_vecs[&*i]->out;
@@ -258,22 +262,8 @@ namespace {
                     allVecs.i_vecs[&*j]->in = UnionOfDifference(allVecs.i_vecs[&*j]->use, allVecs.i_vecs[&*j]->out, allVecs.i_vecs[&*j]->def);
                 } 
             }
-        } 
 
-        virtual bool runOnFunction(Function &F) {
-            bool changed = false;
-            VecL allVecs;
-            vector<Instruction*> removal;
-
-            if(debug) {
-                errs() << "Function: " << F.getName().str() << "\n";
-            }
-
-            livenessAnalysis(&F, allVecs);
-
-            if(debug) {
-                errs() << "Live analysis finished.\n";
-            }
+            // Part 3 : Verify which elements can be safely removed and remove them.
 
             for(Function::iterator i = F.begin(); i != F.end(); i++) {                      // Loop in Basic Blocks 
                 for(BasicBlock::iterator j = i->begin(); j != i->end(); j++) {              // Loop in Instructions  
@@ -309,5 +299,5 @@ namespace {
     }; 
 }
 
-char dcep3::ID = 0;
-static RegisterPass<dcep3> X("dce-p3", "Dead Code Elimination Pass", false, false);
+char livinessP3::ID = 0;
+static RegisterPass<livinessP3> X("la", "Liviness Analysis - DCE", false, false);
